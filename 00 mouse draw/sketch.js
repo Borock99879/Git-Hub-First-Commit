7b0b37094c322dde7916ee729718ea4430eb123a
Borock99879 <ben21sykes@gmail.com>
2026-02-05 10:36:53 -0600

    idk

---
+++ 00 mouse draw/sketch.js
@@ -0,0 +1,13 @@
+// Project Title
+// Your Name
+// Feb 4 2026
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  //background(220);
+  circle(mouseX, mouseY, 40);
+}
