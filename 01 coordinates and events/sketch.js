5d8f5263989ccdee2e17ed6264593bfbee486322
Borock99879 <ben21sykes@gmail.com>
2026-02-06 12:09:06 -0600

    did stuff

--- 01 coordinates and events/sketch.js
+++ 01 coordinates and events/sketch.js
@@ -5,16 +5,28 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let circlex = 175;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 400);
 }
 
 // screen is updated at bottom of draw
 function draw() {
-  background(0); 
+  drawTwoCircles();
+}                                   
+ 
+
+function drawTwoCircles(){
+  background(200, 10 , 20);
+    //     x    y   diameter
+  fill("blue");
+  noStroke();
+  circle(circlex, 100, 75);
+  fill(0);
+  stroke("#AAFFGG");
+  circle(width/2, height/2, 250);
+}
   
-  //     x    y   diameter
-  circle(mouseX, mouseY, 50);}
   
                            
