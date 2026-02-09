d9003a6a96967d16b9b6820402297796bbf7f950
Borock99879 <ben21sykes@gmail.com>
2026-02-09 12:15:19 -0600

    did stuff

--- 02 text and events/sketch.js
+++ 02 text and events/sketch.js
@@ -6,10 +6,40 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let textShade = 255 ;
+let textScale = 40;
+let bgcolor = "grey";
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(bgcolor);
+  //displaymouse();
+  displaykeyboard();
 }
+
+function mousePressed(){
+ 
+  textScale = int(random(10,100));
+  textShade = int(random(0,255));
+}
+
+function displaykeyboard(){
+  if(key === " "){
+    bgcolor = color(random(255), random(255), random(255))
+  }
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  let t = keyIsPressed + ", " + key + ", " + keyCode;
+  text(t, width/2, height/2);
+}
+
+// function displaymouse(){
+
+
+// textSize(textScale);
+// fill(textShade);
+// text(mouseX + ", " + mouseY + ", " + mouseIsPressed + ", " + mouseButton, mouseX, mouseY);
+// }
