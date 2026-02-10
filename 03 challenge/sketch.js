bb5c57cfef2cecfb749c728aa5db585a093b4458
Borock99879 <ben21sykes@gmail.com>
2026-02-10 12:08:06 -0600

    d

---
+++ 03 challenge/sketch.js
@@ -0,0 +1,29 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+ 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  fill("#A8FEB0");
+  stroke("#A8FEB0");
+  circle(500, 500, 200);
+  rect(400 , 500, 200, 150);
+  stroke("#DCDCDC");
+  fill("#DCDCDC");
+  rect(425, 576, 150, 100);
+  fill("black");
+  stroke("black");
+  circle(450,475,20);
+  circle(550,475,20);
+  rect(450,540,100,1);
+  size()
+}
+
