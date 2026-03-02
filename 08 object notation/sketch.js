1c18034f2d0fffa6c4f07f4a7432d54bf2d5c074
Sykes2202622 <2202622@spsd.sk.ca>
2026-03-02 12:14:47 -0600

    did more noise stuff

--- 08 object notation/sketch.js
+++ 08 object notation/sketch.js
@@ -6,10 +6,45 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let ball;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ball = { // object notation. Inside the bracketsset up a bunch of propert : value pairs
+    x: 300, y: 400, size: 20, c: color(random(255), random(255), random(255)), timex: random(100), timey: random(100), timeoff: 0.08
+  }
+  ball2 = { // object notation. Inside the bracketsset up a bunch of propert : value pairs
+    x: 400, y: 300, size: 30, c: color(random(255), random(255), random(255)), timex: random(100), timey: random(100), timeoff: 0.06
+  }
 }
 
 function draw() {
-  background(220);
+ 
+  moveball(ball);
+  moveball(ball2);
+}
+
+function moveball(b){
+  let dx = noise(b.timex);
+  dx = map(dx, 0, 1, -5, 5);
+  let dy = noise(b.timey);
+  dy = map(dy, 0, 1, -5, 5);
+  
+  b.timex += b.timeoff; b.timey += b.timeoff;
+
+  b.x += dx; b.y += dy;
+
+  if(b.x < 0) 
+    b.x = width;
+    else if (b.x > width)
+      b.x = 0;
+    
+    if(b.y < 0) 
+      b.y = height;
+      else if (b.y > height)
+        b.y = 0;
+      
+
+  fill(b.c);
+  circle(b.x, b.y, b.size);
 }
