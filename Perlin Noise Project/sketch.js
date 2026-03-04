967ba0e8b613611e3fa62f417c02f4aeee762cdf
Sykes2202622 <2202622@spsd.sk.ca>
2026-03-04 12:18:08 -0600

    finished, except for drawing average line

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -4,6 +4,11 @@ let x = 0; let w = 1;
 let y = 0; let h = 0;
 let min = 0, max = 0;
 let noisetime = 0, noisespeed = 0.009;
+let move = 0;
+let heightestx = 0;
+let heightesty = 0;
+let averagey = [];
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,37 +16,63 @@ function setup() {
 }
 
 function draw() {
+  noisetime = move;
   background(220);
   drawterrain();
+  drawFlag(heightestx, heightesty);
 }
 
 
 function drawterrain(){
+  averagey = [];
+  heightesty = height;
   fill("black");
   h = height;
   max = height;
-  for (let x = 0; x < width; x += w ){
+  for (let x = 0; x < width; x += w  ){
   y = noise(noisetime);
   y = map(y, 0, 1, min, max);
   rect(x,y,w,h);
 
   noisetime += noisespeed;
+  move += 0.00001
+
+  averagey.push(y);
+  let sum = 0;
+  for (let i = 0; i < averagey.length; i += 1){
+    sum += averagey[i];
+    
   }
+  sum = sum / averagey.length;
+  console.log(sum)
+
+  if (y < heightesty){
+    heightesty = y;
+    heightestx = x;
+  }
+  }
+  
   
 }
 
 function keyPressed(){
   console.log(keyCode);
   if (keyCode === 37){ // left arrow
-    if (w > 2){
+    if (w > 1){
       w += -1;
     }
     
   }
-  else if (keyCode == 39){ // right arrow
-    if (w < 60){
+  else if (keyCode === 39){ // right arrow
+    if (w < 10){
       w += 1;
     }
   }
   
+}
+
+function drawFlag(x,y){
+  fill("#F54927")
+  line(x,y,x,y-20);
+  triangle(x,y-20,x,y-30,x+10,y-25)
 }
