bbd24c1c8d6634158ca4b57b64cf7824082cb25f
Sykes2202622 <2202622@spsd.sk.ca>
2026-03-05 12:17:29 -0600

    done

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -1,14 +1,25 @@
+//terrain generation
+//Ben Sykes
+//3/5/2026
 
 
-let x = 0; let w = 1;
+
+
+
+
+let x = 0; let w = 1;//declares variables for the rectangle values
 let y = 0; let h = 0;
-let min = 0, max = 0;
-let noisetime = 0, noisespeed = 0.009;
-let move = 0;
-let heightestx = 0;
-let heightesty = 0;
-let averagey = [];
 
+let min = 0, max = 0;// sets up values for mapping
+
+let noisetime = 0, noisespeed = 0.009;//handles noise and movement
+let move = 0;
+
+let heightestx = 0;// locates the highest position to draw the flag
+let heightesty = 0;
+
+let averagey = [];// sets up an array and variable to calculate height average
+let sum = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,46 +27,49 @@ function setup() {
 }
 
 function draw() {
-  noisetime = move;
-  background(220);
+  noisetime = move;//updates the noise timeline to add movement
+  background("#78D6FF");
   drawterrain();
   drawFlag(heightestx, heightesty);
+  fill("red");// draws the average line
+  rect(0,sum,width,5);
 }
 
 
 function drawterrain(){
-  averagey = [];
-  heightesty = height;
-  fill("black");
+  averagey = [];// resets the array that calculates average each frame
+  heightesty = height;// sets the heighest point to the botttom as default so any point can be the heighest
+
+  fill("black");//draws all the rectangles
   h = height;
   max = height;
   for (let x = 0; x < width; x += w  ){
-  y = noise(noisetime);
+  y = noise(noisetime);//updates noise to add smoothness
   y = map(y, 0, 1, min, max);
   rect(x,y,w,h);
 
   noisetime += noisespeed;
-  move += 0.00001
+  move += 0.00001// moves the rectangles left by moving the noise timeline
 
-  averagey.push(y);
-  let sum = 0;
+  averagey.push(y);// calculates average by adding items to an array, summing it all up and dividing it by the amount of items
+  sum = 0;//resets the sum so it doesnt mess up future sums
   for (let i = 0; i < averagey.length; i += 1){
-    sum += averagey[i];
+    sum += averagey[i];// adds items to the array
     
   }
-  sum = sum / averagey.length;
-  console.log(sum)
+  sum = sum / averagey.length;// divides the sum by amount of items in array
+  
 
-  if (y < heightesty){
+  if (y < heightesty){//resets the heighest point if a heigher point is located
     heightesty = y;
-    heightestx = x;
+    heightestx = x; // locates the x value of the heighest point
   }
   }
   
   
 }
 
-function keyPressed(){
+function keyPressed(){ // handles increasing the width of the rectangles
   console.log(keyCode);
   if (keyCode === 37){ // left arrow
     if (w > 1){
@@ -64,14 +78,14 @@ function keyPressed(){
     
   }
   else if (keyCode === 39){ // right arrow
-    if (w < 10){
+    if (w < width){
       w += 1;
     }
   }
   
 }
 
-function drawFlag(x,y){
+function drawFlag(x,y){//draws the flag at the heighest point
   fill("#F54927")
   line(x,y,x,y-20);
   triangle(x,y-20,x,y-30,x+10,y-25)
